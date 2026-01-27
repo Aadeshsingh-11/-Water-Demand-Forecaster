361835d4255191fa44236237b52fb57d534fd2af
Aadesh <aadeshsinghad@gmail.com>
2026-01-27 17:31:35 +0530

    Added Flask backend API for water usage prediction

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import flask,request,jsonify 
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Loading model and scaler
+
+model = joblib.load("water_usage_model.pkl")
+scaler = joblib.load("scaler.pkl")
+
+@app.route("/",methods=["GET"])
+def home():
+    return {"message": "Water Demand Forecasting API running"}
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+
+    features = np.array([[
+        data["season"],
+        data["temperature"],
+        data["rainfall"],
+        data["population"],
+        data["year"],
+        data["month"],
+        data["day"],
+        data["dayofweek"]
+    ]])
+
+    features_scaled = scaler.transform(features)
+    prediction = model.predict(features_scaled)
+
+    return jsonify({
+        "predicted_water_usage": float(prediction[0])
+    })
+
+if __name__ == "__main__":
+    app.run()
